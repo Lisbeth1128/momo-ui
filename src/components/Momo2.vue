e80c2012b2b01e343dac404c4210abe5b460c8e0
Lisbeth <13511707@qq.com>
2022-07-06 18:05:42 +0800

    封装 Topnav 组件

--- src/components/Momo2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-      Momo2
-    </div>
-</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,21 @@
 <template>
-    <div>
-      <div class="topnav">
-        <div class="logo">LOGO</div>
-        <ul class="menu">
-          <li>菜单1</li>
-          <li>菜单2</li>
-        </ul>
-      </div>
-      <div class="banner">
-        <h1>墨墨 UI</h1>
-        <h2>一个厉害的 UI 框架</h2>
-        <p class="action">
-            <a href="">GitHub</a>
-            <a href="">开始</a>
-        </p>
-      </div>
+  <div>
+    <Topnav />
+    <div class="banner">
+      <h1>墨墨 UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="action">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </p>
     </div>
+  </div>
 </template>
 
+<script lang="ts">
+  import Topnav from "../components/Topnav.vue"
 
-<style lang="scss" scoped>
-.topnav {
-  background-color: pink;
-  display: flex;
-  padding: 16px;;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
+  export default {
+    components: {Topnav}
   }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em
-    }
-  }
-}
-</style>
+</script>
