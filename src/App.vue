5fbb2d27741bca92e477bda8f48d817b0b54e7af
Lisbeth <13511707@qq.com>
2022-07-10 22:11:53 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { ref, provide } from "vue";
+import { router } from "./router"
 
 const width = document.documentElement.clientWidth
 const menuVisible = ref(width <= 500 ? false : true);
 provide("menuVisible", menuVisible);
+
+router.afterEach(() => {
+  if (width <= 500) {
+    menuVisible.value = false
+  }
+})
 
 </script>
 
