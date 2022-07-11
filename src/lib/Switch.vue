07e32f723b4d3a867d9624410a2801172f00c2bf
Lisbeth <13511707@qq.com>
2022-07-11 15:24:04 +0800

    可以切换了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,28 +1,45 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{ checked }"><span></span></button>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
 button {
-    height: $h;
-    width: $h * 2;
-    border: none;
-    background-color: blue;
-    border-radius: calc($h / 2);
-    position: relative;
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background-color: grey;
+  border-radius: calc($h / 2);
+  position: relative;
 }
 span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background-color: white;
-    border-radius: calc($h2 / 2);
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background-color: white;
+  border-radius: calc($h2 / 2);
 }
-button:hover > span {
-  left: calc(100% - #{$h2} - 2px)
+button.checked {
+  background-color: blue;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
 }
 </style>
