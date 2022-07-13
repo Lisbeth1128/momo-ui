fc00f24a219dc0b486949b7ba54da6f4c7616121
Lisbeth <13511707@qq.com>
2022-07-13 16:11:37 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -18,13 +18,18 @@ export default defineComponent({
       type: String,
       default: "normal",
     },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props) {
-    const { theme, size } = props;
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`momo-theme-${theme}`]: theme,
         [`momo-size-${size}`]: size,
+        [`momo-level-${level}`]: level,
       };
     });
     return { classes };
@@ -38,6 +43,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #30a9ff;
 $radius: 4px;
+$red: red;
 .momo-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -93,6 +100,53 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.momo-theme-button {
+    &.momo-level-main {
+      background-color: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background-color: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.momo-level-danger {
+      background-color: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background-color: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.momo-theme-link {
+    &.momo-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.momo-theme-text {
+    &.momo-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.momo-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
