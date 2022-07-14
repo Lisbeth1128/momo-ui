e1b199b93125276b131e1556ee5da8145d591306
Lisbeth <13511707@qq.com>
2022-07-14 20:30:43 +0800

    遇到一个没办法解决的 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="momo-tabs">
     <div class="momo-tabs-nav">
-      <div class="momo-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+      <div
+        class="momo-tabs-nav-item"
+        v-for="(t, index) in titles"
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+        :key="index"
+      >
         {{ t }}
       </div>
     </div>
     <div class="momo-tabs-content">
-      <component
-        class="momo-tabs-content-item"
-        v-for="(c, index) in defaults"
-        :is="c"
-        :key="index"
-      ></component>
+      {{ current }}
+      <component class="momo-tabs-content-item" :is="current"></component>
     </div>
   </div>
 </template>
@@ -19,8 +21,14 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Tab from "./Tab.vue";
+import { computed } from "vue";
 
 export default defineComponent({
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const slots = context.slots;
 
@@ -31,15 +39,26 @@ export default defineComponent({
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        if(tag.props === null) {return}
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag) => {
       if (tag.props === null) {
         return;
       }
       return tag.props.title;
     });
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
       titles,
+      current,
+      select
     };
   },
 });
