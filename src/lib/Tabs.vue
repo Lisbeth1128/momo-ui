85ad9c0f26b57873504daa68beabcab5a6267414
Lisbeth <13511707@qq.com>
2022-07-14 17:23:42 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,19 @@
 <template>
-  <div v-for="(t, index) in titles" :key="index">
-    {{ t }}
+  <div class="momo-tabs">
+    <div class="momo-tabs-nav">
+      <div class="momo-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="momo-tabs-content">
+      <component
+        class="momo-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      ></component>
+    </div>
   </div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
 </template>
 
 <script lang="ts">
@@ -33,3 +44,31 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.momo-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
