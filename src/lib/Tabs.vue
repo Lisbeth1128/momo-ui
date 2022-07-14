9661d9e0dca7e9697dd5f6250f81a77168350f05
Lisbeth <13511707@qq.com>
2022-07-15 01:51:14 +0800

    优化 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,7 +4,7 @@
       <div
         class="momo-tabs-nav-item"
         v-for="(t, index) in titles"
-        :ref="el => { if (el) navItems[index] = el }"
+        :ref="el => { if (t === selected) selectedItem = el }"
         @click="select(t)"
         :class="{ selected: t === selected }"
         :key="index"
@@ -31,22 +31,20 @@ export default defineComponent({
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
       const {
         width
-      } = result.getBoundingClientRect()
+      } = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const {
         left: left1
       } = container.value.getBoundingClientRect()
       const {
         left: left2
-      } = result.getBoundingClientRect()
+      } = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -82,7 +80,7 @@ export default defineComponent({
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
