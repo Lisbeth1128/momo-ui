bead22120e97cc8691c25a08fd3dddfbbc1960a9
Lisbeth <13511707@qq.com>
2022-07-15 17:04:51 +0800

    使用本地的 svg.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import "./lib/momo.scss"
-import App from "./App.vue";
 import "./index.scss";
+import App from "./App.vue";
 import { router } from "./router"
+import "./lib/svg.js"
 
 
 const app = createApp(App);
