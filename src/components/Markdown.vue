709ac33496de8959c853709d4e660a2c8e69c815
Lisbeth <13511707@qq.com>
2022-07-22 00:51:56 +0800

    解决上次的遗留问题

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -7,18 +7,9 @@ import { ref } from "vue";
 
 export default {
     props: {
-        path: {
+        content: {
             type: String,
             default: true
-        }
-    },
-    setup(props) {
-        const content = ref<string>(null)
-        import(props.path).then(result => {
-            content.value = result.default
-        })
-        return {
-            content
         }
     }
 }
